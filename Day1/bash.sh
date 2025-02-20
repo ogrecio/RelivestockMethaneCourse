ae9c8d2ef79fe41cf4ce382c0934d55ecae60e0a
Coralia Manzanilla Pech <coralia.manzanillapech@wur.nl>
2025-02-20 15:54:50 +0100

    bash.sh

--- Day1/bash.sh
+++ Day1/bash.sh
@@ -4,4 +4,4 @@ ml use /lustre/shared/Courses/RELIVESTOCK2025/modules
 
 module load SnifferAnalyzer
 
-sniffer_analyzer herd_robot101.csv herd_sniffer101_Loggy_42s_FD1.txt 42
+sniffer_analyzer herd_robot101.csv herd_sniffer101_Loggy_48sg.txt 42
